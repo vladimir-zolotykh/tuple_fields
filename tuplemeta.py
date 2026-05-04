e1a0279262afacb262c6e4817f2b9886ff86fd40
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-04 20:24:10 +0300

    Use property() [TupleMeta.__init__ ]
    -            setattr(cls, field, itemgetter(i))
    +            setattr(cls, field, property(itemgetter(i)))

--- tuplemeta.py
+++ tuplemeta.py
@@ -8,7 +8,7 @@ class TupleMeta(type):
     def __init__(cls, clsname, bases, clsdict, **kwargs):
         fields = clsdict.get("fields", [])
         for i, field in enumerate(fields):
-            setattr(cls, field, itemgetter(i))
+            setattr(cls, field, property(itemgetter(i)))
 
 
 class Tuple(tuple, metaclass=TupleMeta):
@@ -29,6 +29,7 @@ class Exercise(Tuple):
 if __name__ == "__main__":
     # p = Point(10, 12, 13)
     p = Point(10, 12)
+    print(p.x)
     print(p)
     e = Exercise("Squat", 90.0, 3)
     print(e)
