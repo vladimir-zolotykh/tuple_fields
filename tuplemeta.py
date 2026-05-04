32743dd86e13041f5434f2ceea0a177ba5be824b
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-04 18:44:10 +0300

    Add Tuple.__new__() method

--- tuplemeta.py
+++ tuplemeta.py
@@ -12,8 +12,10 @@ class TupleMeta(type):
 
 
 class Tuple(tuple, metaclass=TupleMeta):
-    def __new__(self, *args, **kwargs):
-        return super().__new__(*args)
+    def __new__(cls, *args, **kwargs):
+        if len(args) != len(cls.fields):
+            raise TypeError(f"Must supply {len(cls.fields)} args")
+        return super().__new__(cls, args)
 
 
 class Point(Tuple):
@@ -25,6 +27,7 @@ class Exercise(Tuple):
 
 
 if __name__ == "__main__":
+    # p = Point(10, 12, 13)
     p = Point(10, 12)
     print(p)
     e = Exercise("Squat", 90.0, 3)
